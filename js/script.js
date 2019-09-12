fb1381e049e0c6ebecb19f002cfec8089c2d962f
zaraepi <zaharah.epifantseva@yahoo.com>
2019-09-12 15:19:24 +1200

    new commit

--- js/script.js
+++ js/script.js
@@ -18,12 +18,12 @@ $(function() {
         offset5 = $('.section5').offset().top;
         // offsetsectionartwork = $('#section-artwork').offset().top;
 
-         console.log(offset1);
-         console.log(offset2);
-         console.log(offset3);
-         console.log(offset4);
-         console.log(offset5);
-         // console.log(offsetsectionartwork);
+        console.log(offset1);
+        console.log(offset2);
+        console.log(offset3);
+        console.log(offset4);
+        console.log(offset5);
+        // console.log(offsetsectionartwork);
 
         // console.log('---------------');
 
@@ -116,27 +116,28 @@ $(function() {
 
         var activeLi;
 
-        var iScrollTop = iRealScrollTop + 151;
+        var iScrollTop = iRealScrollTop + 300;
+        // original is 151 not 300, on sept 9 2019
 
         if ((iScrollTop >= offset1) && (iScrollTop < offset2)) {
             //looking at section 1
             activeLi = $('.menu-list>li:nth-child(1)');
-           
+
         }
         if ((iScrollTop >= offset2) && (iScrollTop < offset3)) {
             //looking at section 2
             activeLi = $('.menu-list>li:nth-child(2)');
-            
+
         }
         if ((iScrollTop >= offset3) && (iScrollTop < offset4)) {
             //looking at section 3
             activeLi = $('.menu-list>li:nth-child(3)');
-            
+
         }
         if ((iScrollTop >= offset4) && (iScrollTop < offset5)) {
             //looking at section 4
             activeLi = $('.menu-list>li:nth-child(4)');
-             
+
         }
 
 
@@ -144,7 +145,7 @@ $(function() {
             // looking at section 5
             activeLi = $('.menu-list>li:nth-child(5)');
 
-            
+
         }
 
 
@@ -156,10 +157,7 @@ $(function() {
     });
 
 
-// testing to hide menu list when click on menu li
-// $('.menu-list>li').on('click',function(e){
-// $('#slide-button').hide('');
-// });
+
 
 
     //------------smooth scrolling---------------------------
@@ -173,7 +171,7 @@ $(function() {
 
         console.log(offsetTop);
         $('body,html').animate({
-            scrollTop: offsetTop - 150 //i change this so that i can see title between section, i fixed this too yay
+            scrollTop: offsetTop - 150 //i change this 
         }, 1000); //html,body is for jqeury to select th whole thing
 
     });
@@ -189,7 +187,7 @@ $(function() {
 
         console.log(offsetTop);
         $('body,html').animate({
-            scrollTop: offsetTop - 150 //i change this so that i can see title between section, i fixed this too yay
+            scrollTop: offsetTop - 150 //i change this 
         }, 1000); //html,body is for jqeury to select th whole thing
 
     });
@@ -205,7 +203,7 @@ $(function() {
 
         console.log(offsetTop);
         $('body,html').animate({
-            scrollTop: offsetTop - 150 //i change this so that i can see title between section, i fixed this too yay
+            scrollTop: offsetTop - 150 //i change this 
         }, 1000); //html,body is for jqeury to select th whole thing
 
     });
@@ -215,309 +213,6 @@ $(function() {
 
 
 
-
-    //--------calculation section 2------------------
-
-    // $('.builder-tick-feedback').hide();
-    // $('.down').hide();
-    // $('.up').show();
-    // $('#down-input').show();
-    // $('#up-input').hide();
-    // updateOffsetTops();
-
-    // $('#search-btn').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     var sGuests = $('#guests').val();
-    //     var iGuests = parseInt(sGuests);
-
-    //     var sDays = $('#days').val();
-    //     var iDays = parseInt(sDays);
-
-    //     var iPriceA = 0;
-    //     var iPriceB = 0;
-    //     var iPriceC = 0;
-    //     var iPriceD = 0;
-
-    //     $('.hotelA').attr("checked", false);
-    //     $('.hostelB').attr("checked", false);
-    //     $('.motelC').attr("checked", false);
-    //     $('.houseD').attr("checked", false);
-    //     $('.A').attr("checked", false);
-    //     $('.B').attr("checked", false);
-    //     $('.C').attr("checked", false);
-    //     $('.D').attr("checked", false);
-    //     $('.accomodation-output').html(0);
-    //     $('.meal-output').html(0);
-    //     $('.total-output').html(0);
-
-
-    //     $('.test').addClass('disable-container');
-
-    //     if ((iGuests >= 1) && (iGuests <= 2) && (iDays >= 1) && (iDays <= 5)) {
-    //         $('.container-A').removeClass('disable-container');
-    //         console.log('hi');
-    //         $('.planA').addClass('enable');
-    //         iPriceA = 157 * iDays;
-    //         $('#priceA').html('NZD ' + iPriceA);
-    //         $('.hotelA').attr("disabled", false);
-
-    //     } else {
-    //         $('.planA').removeClass('enable');
-    //         $('#priceA').html('');
-    //         $('.hotelA').attr("disabled", true);
-
-
-    //     }
-
-    //     if ((iGuests == 1) && (iDays >= 1) && (iDays <= 10)) {
-    //         $('.container-B').removeClass('disable-container');
-    //         $('.planB').addClass('enable');
-    //         iPriceB = 30 * iDays;
-    //         $('#priceB').html('NZD ' + iPriceB);
-    //         $('.hostelB').attr("disabled", false);
-
-    //     } else {
-    //         $('.planB').removeClass('enable');
-    //         $('#priceB').html('');
-    //         $('.hostelB').attr("disabled", true);
-
-    //     }
-
-    //     if ((iGuests >= 2) && (iGuests <= 4) && (iDays >= 3) && (iDays <= 10)) {
-    //         $('.container-C').removeClass('disable-container');
-    //         $('.planC').addClass('enable');
-    //         iPriceC = 90 * iDays;
-    //         $('#priceC').html('NZD ' + iPriceC);
-    //         $('.motelC').attr("disabled", false);
-
-    //     } else {
-    //         $('.planC').removeClass('enable');
-    //         $('#priceC').html('');
-    //         $('.motelC').attr("disabled", true);
-
-    //     }
-
-    //     if ((iGuests >= 1) && (iGuests <= 4) && (iDays >= 2) && (iDays <= 15)) {
-    //         $('.container-D').removeClass('disable-container');
-    //         $('.planD').addClass('enable');
-    //         iPriceD = 240 * iDays;
-    //         $('#priceD').html('NZD ' + iPriceD);
-    //         $('.houseD').attr("disabled", false);
-
-    //     } else {
-    //         $('.planD').removeClass('enable');
-    //         $('#priceD').html('');
-    //         $('.houseD').attr("disabled", true);
-
-    //     }
-    //     if ((iGuests >= 1) && (iGuests <= 4) && (iDays >= 1) && (iDays <= 15)) {
-    //         $('.holiday-accordian').hide();
-    //         $('.home-plan-accordian').slideDown('slow');
-    //         $('body').scrollTop($('.text-main2').offset().top - 150);
-    //         $('#down-input').hide();
-    //         $('#builder-tick-input').show();
-    //         $('#up-hotel').hide();
-    //         $('#down-hotel').show();
-
-    //     }
-    // });
-
-
-
-    //------open close based on flow and validation----no jumping for user---
-    //------first the holiday plan must open------
-
-    // $('.holiday-accordian').removeClass('builder-accordian_body'); //pls unhide later
-    // $('.builder-accordian_body').hide(); //pls unhide later
-
-    // updateOffsetTops();
-
-
-
-
-    //------next-btn from home section-----
-
-    // $('#next-btn-home').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     var isHotelChecked = $('#hotelA').is(':checked') || $('#hostelB').is(':checked') || $('#motelC').is(':checked') || $('#houseD').is(':checked');
-
-    //     if (isHotelChecked == true) {
-    //         $('.home-plan-accordian').hide();
-    //         $('.meal-plan-accordian').slideDown('slow');
-    //         $('body').scrollTop($('.text-main2').offset().top - 150);
-    //         $('#down-hotel').hide();
-    //         $('#builder-tick-hotel').show();
-    //         $('#up-meal').hide();
-    //         $('#down-meal').show();
-
-    //     }
-
-
-    // });
-
-    //---next-btn from meal section-----
-
-    // $('#next-btn-meal').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     $('.meal-plan-accordian').hide();
-    //     $('.cost-plan-accordian').slideDown('slow');
-    //     $('body').scrollTop($('.text-main2').offset().top - 150);
-    //     $('#down-meal').hide();
-    //     $('#builder-tick-meal').show();
-    //     $('#up-cost').hide();
-    //     $('#down-cost').show();
-
-
-    // });
-
-    // $('#back-btn-cost').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     $('.cost-plan-accordian').hide();
-    //     $('.meal-plan-accordian').slideDown('slow');
-    //     $('body').scrollTop($('.text-main2').offset().top - 150);
-    //     $('#builder-tick-meal').hide();
-    //     $('#down-meal').show();
-    //     $('#down-cost').hide();
-    //     $('#up-cost').show();
-
-    // });
-
-    // $('#back-btn-meal').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     $('.meal-plan-accordian').hide();
-    //     $('.home-plan-accordian').show();
-    //     $('body').scrollTop($('.text-main2').offset().top - 150);
-    //     $('#builder-tick-hotel').hide();
-    //     $('#down-hotel').show();
-    //     $('#down-meal').hide();
-    //     $('#up-meal').show();
-
-    // });
-
-    // $('#back-btn-home').on('click', function(event) {
-    //     event.preventDefault();
-
-    //     $('.home-plan-accordian').hide();
-    //     $('.holiday-accordian').show();
-    //     $('body').scrollTop($('.text-main2').offset().top - 150);
-    //     $('#builder-tick-input').hide();
-    //     $('#down-input').show();
-    //     $('#down-hotel').hide();
-    //     $('#up-hotel').show();
-    //     $('.meal-plan-accordian').slideUp();
-
-
-
-    // });
-
-
-
-    //----------total accomodation+meal=totalcost---------
-
-    // $.fn.updateTotal = function() {
-
-    //     var sHotelPrice = $('.accomodation-output').html();
-    //     var iHotelPrice = parseInt(sHotelPrice);
-    //     var sMealPrice = $('.meal-output').html();
-    //     var iMealPrice = parseFloat(sMealPrice);
-
-    //     var iTotalPrice = iHotelPrice + iMealPrice;
-    //     $('.total-output').html(iTotalPrice);
-    // }; //added semicolon here
-
-    //----------accomodation output-------
-    // $.fn.updateAccomodation = function() {
-    //     var sDays = $('#days').val();
-    //     var iDays = parseInt(sDays);
-    //     var iAccomodationPrice = $('input[name="accomodation"]:checked').val() * iDays;
-    //     $('.accomodation-output').html(iAccomodationPrice);
-    //     $.fn.updateTotal();
-    // }; //added semicolon here
-    // $('.hotelA').on('click', function() {
-    //     $.fn.updateAccomodation();
-    // });
-    // $('.hostelB').on('click', function() {
-    //     $.fn.updateAccomodation();
-    // });
-
-    // $('.motelC').on('click', function() {
-    //     $.fn.updateAccomodation();
-    // });
-
-    // $('.houseD').on('click', function() {
-    //     $.fn.updateAccomodation();
-    // });
-
-    //-------meal output----------
-    // $.fn.updateMeal = function() {
-    //     var sGuests = $('#guests').val();
-    //     var iGuests = parseInt(sGuests);
-    //     var sDays = $('#days').val();
-    //     var iDays = parseInt(sDays);
-
-        // var iMealPrice = parseFloat(sVal); //this is added today
-        // var iMealPrice = $('input[name="meal"]:checked').val() * iGuests * iDays;
-
-        // console.log(iMealPrice);
-    //     $('.meal-output').html(iMealPrice);
-    //     $.fn.updateTotal();
-    // }; //added semicolon here
-
-    // $('.A').on('click', function() {
-    //     $.fn.updateMeal();
-    // });
-    // $('.B').on('click', function() {
-    //     $.fn.updateMeal();
-    // });
-
-    // $('.C').on('click', function() {
-    //     $.fn.updateMeal();
-    // });
-
-    // $('.D').on('click', function() {
-    //     $.fn.updateMeal();
-    // });
-    //-------------------
-
-
-
-
-    //------form validation this is jquery plugin------
-    // console.log('bla');
-
-    // $('#builder-validate').validate({
-    //     rules: {
-    //         guests: {
-    //             required: true,
-    //             range: [1, 4]
-
-    //         },
-    //         days: {
-    //             required: true,
-    //             range: [1, 15]
-    //         },
-
-    //     },
-
-    //     messages: {
-    //         guests: {
-    //             required: 'Please enter number only',
-    //             range: 'Must be less than 4 person'
-    //         },
-    //         days: {
-    //             required: 'Please enter number only',
-    //             range: 'Must be max 15 days only'
-    //         }
-
-    //     }
-
-
-    // });
 
 
 
@@ -588,7 +283,7 @@ $(function() {
 
 
     //-----section2 ReadMore-B-button-left ive added this--------
-        $('.ReadMore-description-left-B').hide();
+    $('.ReadMore-description-left-B').hide();
     updateOffsetTops();
 
     var isOpenReadmoreB = false; //var removed
@@ -609,7 +304,7 @@ $(function() {
     });
 
     //-----section2 ReadMore-C-button-left ive added this--------
-        $('.ReadMore-description-left-C').hide();
+    $('.ReadMore-description-left-C').hide();
     updateOffsetTops();
 
     var isOpenReadmoreC = false; //var removed
@@ -630,7 +325,7 @@ $(function() {
     });
 
     //-----section2 ReadMore-D-button-left ive added this--------
-        $('.ReadMore-description-left-D').hide();
+    $('.ReadMore-description-left-D').hide();
     updateOffsetTops();
 
     var isOpenReadmoreD = false; //var removed
@@ -656,7 +351,7 @@ $(function() {
     //---location icon section when click will go to section 5 contact to see map-----
 
 
-     //------------img1 moved in and other images to move in on section 2 container ive added this------------
+    //------------img1 moved in and other images to move in on section 2 container ive added this------------
 
     $(document).on('scroll', function() {
 
@@ -665,9 +360,10 @@ $(function() {
             var iScrollTop = $(document).scrollTop();
 
             var offsetTop = $(this).offset().top;
-// i change it to 600, original is 300
-            if (iScrollTop > offsetTop - 300) {
+            // i change it to 600, original is 300
+            if (iScrollTop > offsetTop - 800) {
                 $(this).addClass('moved-in');
+
             } else {
                 $(this).removeClass('moved-in');
 
@@ -678,6 +374,26 @@ $(function() {
         });
     });
 
+
+    $(document).on('scroll', function() {
+
+        $('.title').each(function(i, el) {
+
+            var iScrollTop = $(document).scrollTop();
+
+            var offsetTop = $(this).offset().top;
+            // i change it to 600, original is 300
+            if (iScrollTop > offsetTop - 800) {
+                $(this).addClass('moved-in');
+            } else {
+                $(this).removeClass('moved-in');
+
+
+            }
+
+
+        });
+    });
 
 
 
@@ -772,76 +488,36 @@ $(function() {
 
 
 
-    //-----------swiper plugin-section4-----
-
-    // $(document).ready(function () {
-    //initialize swiper when document ready  
-    // var mySwiper = new Swiper('.swiper-container', {
-    //         // Optional parameters
-    //         direction: 'vertical',
-    //         loop: true
-    //     })
-    // });
-
-
-
-// enable this later below
-    // var swiper = new Swiper('.swiper-container', {
-    //     pagination: '.swiper-pagination',
-    //     paginationClickable: true,
-    //     slidesPerView: 5,
-    //     spaceBetween: 50,
-    //     breakpoints: {
-    //         1024: {
-    //             slidesPerView: 4,
-    //             spaceBetween: 40
-    //         },
-    //         768: {
-    //             slidesPerView: 3,
-    //             spaceBetween: 30
-    //         },
-    //         640: {
-    //             slidesPerView: 2,
-    //             spaceBetween: 20
-    //         },
-    //         320: {
-    //             slidesPerView: 1,
-    //             spaceBetween: 10
-    //         }
-    //     }
-    // }); enable this later
 
 
 
 
-
-
-     var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 5,
-      spaceBetween: 50,
-      // init: false,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 40,
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 50,
+        // init: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10,
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            }
         }
-      }
     });
 
     //---------swiper 2-----------------
@@ -856,17 +532,10 @@ $(function() {
     updateOffsetTops();
 
 
+    // ive added this plugin for smooth scrolling image reveal effect!
+    AOS.init({
+        duration: 1200,
+    });
+    //
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
